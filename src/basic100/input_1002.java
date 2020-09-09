e46cc5cef74cd2caf089bed3e8b8ca838eebd84e
dofany <kimdohwan17@gmail.com>
2020-09-09 16:50:42 +0900

    기초 100제

--- src/basic100/input_1002.java
+++ src/basic100/input_1002.java
@@ -2,6 +2,6 @@ package basic100;
 
 public class input_1002 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello\nWorld");
     }
 }
