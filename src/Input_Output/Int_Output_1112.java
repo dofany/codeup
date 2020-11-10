76c17687e7eeb2b691e961b23817e6ba0f622cd4
dofany <kimdohwan17@gmail.com>
2020-11-10 18:07:03 +0900

    두 정수 출력

---
+++ src/Input_Output/Int_Output_1112.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Int_Output_1112 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d %d",a,b);
+    }
+}
