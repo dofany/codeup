0f4775507504d256a166db7a3d37f4dbc3ca7019
dofany <kimdohwan17@gmail.com>
2020-11-10 18:39:05 +0900

    나머지 구하기

---
+++ src/Input_Output/Remainder_1121.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Remainder_1121 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a%b);
+    }
+}
