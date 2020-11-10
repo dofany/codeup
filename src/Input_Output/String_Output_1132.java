25ff3912c29a70dbaa08e7042de21d0a85a161cc
dofany <kimdohwan17@gmail.com>
2020-11-10 18:58:01 +0900

    문자열 출력하기

---
+++ src/Input_Output/String_Output_1132.java
@@ -0,0 +1,11 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class String_Output_1132 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
