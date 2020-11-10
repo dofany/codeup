46b1a9b488251870c320cf1163d40f7a89af3a2b
dofany <kimdohwan17@gmail.com>
2020-11-10 18:24:03 +0900

    두 실수의 곱

---
+++ src/Input_Output/Multi_1117.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Multi_1117 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
