adb606bc3981a7bc90fddd850b715163c3d18b56
dofany <kimdohwan17@gmail.com>
2020-11-10 18:04:43 +0900

    %출력

---
+++ src/Input_Output/Output_1111.java
@@ -0,0 +1,11 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Output_1111 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a+"%");
+    }
+}
