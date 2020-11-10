446049ec63b12604fdfbb529a1ceda9d6b903c06
dofany <kimdohwan17@gmail.com>
2020-11-10 20:12:35 +0900

    두 수의 대소 비교

---
+++ src/if_else/Compare_1153.java
@@ -0,0 +1,19 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class Compare_1153 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>b){
+            System.out.println(">");
+        } else if(b>a){
+            System.out.println("<");
+        } else if(a==b){
+            System.out.println("=");
+        }
+    }
+}
