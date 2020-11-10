069a878d86f73c37e1ae83325c8533da3783369d
dofany <kimdohwan17@gmail.com>
2020-11-10 18:54:43 +0900

    8진수 16진수 변환

---
+++ src/Input_Output/Binary_Change_1125.java
@@ -0,0 +1,11 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Binary_Change_1125 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%o %X",a,a);
+    }
+}
