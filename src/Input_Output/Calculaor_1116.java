493d424e2a0e8916a36305b087c012872cec4e85
dofany <kimdohwan17@gmail.com>
2020-11-10 18:20:16 +0900

    사칙연산 계산기

---
+++ src/Input_Output/Calculaor_1116.java
@@ -0,0 +1,17 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Calculaor_1116 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d+%d=%d\n",a,b,a+b);
+        System.out.printf("%d-%d=%d\n",a,b,a-b);
+        System.out.printf("%d*%d=%d\n",a,b,a*b);
+        System.out.printf("%d/%d=%d\n",a,b,a/b);
+
+
+    }
+}
