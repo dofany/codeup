ba373b4dfb5b2ae35203fae52b8c3dd61639d277
dofany <kimdohwan17@gmail.com>
2020-11-10 19:14:47 +0900

    논리 연산자(NOT)

---
+++ src/Input_Output/Logical_NOT_1138.java
@@ -0,0 +1,15 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Logical_NOT_1138 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
