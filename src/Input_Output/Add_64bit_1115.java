6349a94ec003a56407ab851ae4222864fa32299d
dofany <kimdohwan17@gmail.com>
2020-11-10 18:16:07 +0900

    두 정수의 덧셈(64bit)

---
+++ src/Input_Output/Add_64bit_1115.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Add_64bit_1115 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(a+b);
+    }
+}
