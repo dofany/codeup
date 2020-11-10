3d92453c4c933a29ed7afd951a8ef0f5772c05e1
dofany <kimdohwan17@gmail.com>
2020-11-10 19:10:43 +0900

    관계연산자3

---
+++ src/Input_Output/Operator3_1137.java
@@ -0,0 +1,16 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Operator3_1137 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == b) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
+    }
+}
