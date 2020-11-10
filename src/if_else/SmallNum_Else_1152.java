babe33763dfb9cf3a3200b26f24d6fa5759f26a6
dofany <kimdohwan17@gmail.com>
2020-11-10 20:04:51 +0900

    10보다 작은 수(else 버전)

---
+++ src/if_else/SmallNum_Else_1152.java
@@ -0,0 +1,15 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class SmallNum_Else_1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(10>a){
+            System.out.println("small");
+        } else{
+            System.out.println("big");
+        }
+    }
+}
