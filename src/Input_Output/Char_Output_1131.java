01547f53abefe51f9c0040ce580b63ad2c3f28d9
dofany <kimdohwan17@gmail.com>
2020-11-10 18:56:52 +0900

    문자 출력하기

---
+++ src/Input_Output/Char_Output_1131.java
@@ -0,0 +1,11 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Char_Output_1131 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println(c);
+    }
+}
