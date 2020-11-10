2319856e353f74ddc458e1b6877a3ac41ca4c256
dofany <kimdohwan17@gmail.com>
2020-11-10 19:59:10 +0900

    10보다 작은 수

---
+++ src/if_else/SmallNum_1151.java
@@ -0,0 +1,12 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class SmallNum_1151 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(10>a)
+            System.out.println("small");
+    }
+}
