affcbb3da189b514411a10f43ecbf4090ec03e1e
dofany <kimdohwan17@gmail.com>
2020-11-10 18:26:51 +0900

    삼각형의 넓이 구하기

---
+++ src/Input_Output/Triangle_1118.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Triangle_1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print((float)a*b/2);
+    }
+}
