f1048a44713f62b6a9a11819651b3584881bfcde
dofany <kimdohwan17@gmail.com>
2020-11-10 19:36:50 +0900

    비트 연산자(>>)

---
+++ src/Input_Output/Bitwise_Right_1148.java
@@ -0,0 +1,12 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Bitwise_Right_1148 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a>>b);
+    }
+}
