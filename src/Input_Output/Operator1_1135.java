6ea2e1f2d9cfd3062985b1ea1ecf6d19e08c58cc
dofany <kimdohwan17@gmail.com>
2020-11-10 19:04:54 +0900

    관계연산자1

---
+++ src/Input_Output/Operator1_1135.java
@@ -0,0 +1,16 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Operator1_1135 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>=b){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+    }
+}
