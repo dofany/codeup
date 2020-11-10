ef9534f8e6874be22cfdfe08baffa7c2a0282325
dofany <kimdohwan17@gmail.com>
2020-11-10 19:01:17 +0900

    공백이 있는 문자열 입출력

---
+++ src/Input_Output/SpritString_1133.java
@@ -0,0 +1,13 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class SpritString_1133 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+
+    }
+}
+
