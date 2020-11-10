15eff961e218228bf9ccd2b30eecd6ad63679330
dofany <kimdohwan17@gmail.com>
2020-11-10 18:29:13 +0900

    일을 시간으로 변환

---
+++ src/Input_Output/Day_Time_1119.java
@@ -0,0 +1,11 @@
+package Input_Output;
+
+import java.util.Scanner;
+
+public class Day_Time_1119 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*24);
+    }
+}
