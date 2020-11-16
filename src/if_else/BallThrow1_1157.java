732e3d882498373033e368000fa47838b2803de7
dofany <kimdohwan17@gmail.com>
2020-11-16 18:32:42 +0900

    특별한 공 던지기 1

---
+++ src/if_else/BallThrow1_1157.java
@@ -0,0 +1,15 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class BallThrow1_1157 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        if(a>=50 && a<=60){
+            System.out.println("win");
+        } else{
+            System.out.println("lose");
+        }
+    }
+}
