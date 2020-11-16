09ca74324adae5b92d37ca243afc10fba0e09219
dofany <kimdohwan17@gmail.com>
2020-11-16 19:53:13 +0900

    아르바이트 가는 날

---
+++ src/if_else/ParttimeJob_1160.java
@@ -0,0 +1,18 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class ParttimeJob_1160 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        switch (a){
+            case 1: case 3: case 5: case 7:
+                System.out.println("oh my god");
+                break;
+            case 2: case 4: case 6:
+                System.out.println("enjoy");
+                break;
+        }
+    }
+}
