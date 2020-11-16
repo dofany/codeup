d0e603f54879db03bc4568b303a709e7d8b1f468
dofany <kimdohwan17@gmail.com>
2020-11-16 21:19:26 +0900

    축구의 신 1

---
+++ src/if_else/SoccerGod1_1165.java
@@ -0,0 +1,13 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class SoccerGod1_1165 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = ((89-a)/5)+1;
+        System.out.println(b+result);
+    }
+}
