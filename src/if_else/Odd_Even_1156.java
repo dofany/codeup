1a716bdb1965ed642aef14fb06e0105998e341dd
dofany <kimdohwan17@gmail.com>
2020-11-16 18:28:42 +0900

    홀수 짝수 구별

---
+++ src/if_else/Odd_Even_1156.java
@@ -0,0 +1,15 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class Odd_Even_1156 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%2==0){
+            System.out.println("even");
+        } else{
+            System.out.println("odd");
+        }
+    }
+}
