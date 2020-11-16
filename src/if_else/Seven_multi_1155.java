28a705523827094154e40ecc4b29d5122a8d2fab
dofany <kimdohwan17@gmail.com>
2020-11-16 18:26:44 +0900

    7의 배수

---
+++ src/if_else/Seven_multi_1155.java
@@ -0,0 +1,15 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class Seven_multi_1155 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 7 == 0){
+            System.out.println("multiple");
+        } else{
+            System.out.println("not multiple");
+        }
+    }
+}
