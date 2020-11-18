71bce6b63574d5dad9bf56190302af2ebd65b585
dofany <kimdohwan17@gmail.com>
2020-11-18 09:14:10 +0900

    컨설팅 회사

---
+++ src/if_else/Company_1216.java
@@ -0,0 +1,20 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class Company_1216 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a<(b-c)){
+            System.out.println("advertise");
+        } else if(a>(b-c)){
+            System.out.println("do not advertise");
+        } else{
+            System.out.println("does not matter");
+        }
+    }
+}
