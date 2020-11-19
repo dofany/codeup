0caa07d2ae71e659edc32dc03dcd0abb3cfb14bd
dofany <kimdohwan17@gmail.com>
2020-11-19 12:18:34 +0900

    1부터 100까지 출력하기

---
+++ src/simple_for/One_Onehundred_1251.java
@@ -0,0 +1,11 @@
+package simple_for;
+
+public class One_Onehundred_1251 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 101; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
