7cae8833e64be7ddd8c8b5c58f370be89b8edf18
dofany <kimdohwan17@gmail.com>
2020-11-19 12:20:23 +0900

    1부터 n까지 출력하기

---
+++ src/simple_for/One_n_1252.java
@@ -0,0 +1,13 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class One_n_1252 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
