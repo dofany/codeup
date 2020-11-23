313f90a9790391f95db9ba6ad1263603c50d2a09
dofany <kimdohwan17@gmail.com>
2020-11-23 20:29:05 +0900

    별 출력하기

---
+++ src/simple_for/Star_1256.java
@@ -0,0 +1,13 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class Star_1256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            System.out.print("*");
+        }
+    }
+}
