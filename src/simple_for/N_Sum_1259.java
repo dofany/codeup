6dccaa5e2f56fedabccf1f5c6036aaa150a64eff
dofany <kimdohwan17@gmail.com>
2020-11-23 20:58:46 +0900

    1부터 n까지 중 짝수의 합 구하기

---
+++ src/simple_for/N_Sum_1259.java
@@ -0,0 +1,17 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class N_Sum_1259 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i<=n; i++){
+            if(i%2==0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
