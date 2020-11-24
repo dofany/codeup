b5765e7c442460955c88f205dcfeb42f5c9621a3
dofany <kimdohwan17@gmail.com>
2020-11-24 21:01:35 +0900

    자릿수 계산

---
+++ src/simple_for/Digit_1278.java
@@ -0,0 +1,12 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class Digit_1278 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int word = s.length();
+        System.out.println(word);
+    }
+}
