5af137344af53498513a08884225cb5e0d7de710
dofany <kimdohwan17@gmail.com>
2020-11-24 19:11:20 +0900

    k제곱 구하기

---
+++ src/simple_for/Squared_1275.java
@@ -0,0 +1,12 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class Squared_1275 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print((int)Math.pow(a,b));
+    }
+}
