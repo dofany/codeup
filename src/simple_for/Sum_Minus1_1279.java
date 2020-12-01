440b643609123395e27e44cc9259d5ae8d451ec7
dofany <kimdohwan17@gmail.com>
2020-12-01 18:20:39 +0900

    홀수는 더하고 짝수는 빼고 1

---
+++ src/simple_for/Sum_Minus1_1279.java
@@ -0,0 +1,21 @@
+package simple_for;
+
+import java.util.Scanner;
+
+public class Sum_Minus1_1279 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = 0;
+
+        for(int i = a; i<=b; i++){
+            if(i%2==0){
+                result -= i;
+            }else{
+                result += i;
+            }
+        }
+        System.out.println(result);
+    }
+}
