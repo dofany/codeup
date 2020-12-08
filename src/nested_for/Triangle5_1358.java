b6ac77788a469d65f7e9f22a567bbf9b95af9856
dofany <kimdohwan17@gmail.com>
2020-12-08 14:42:20 +0900

    삼각형 출력하기 5

---
+++ src/nested_for/Triangle5_1358.java
@@ -0,0 +1,27 @@
+package nested_for;
+
+import java.util.Scanner;
+
+public class Triangle5_1358 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i<(n/2)+1; i++){
+            for(int j = (n/2)+1; j>0; j--){
+                if(j>i+1){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            for(int j = 0; j<=(n/2)-1; j++){
+                if(j<i){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
