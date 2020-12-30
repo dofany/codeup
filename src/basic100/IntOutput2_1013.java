110904f56c7b3eba216a631f7b29b50d8aa9a740
dofany <kimdohwan17@gmail.com>
2020-12-30 14:34:23 +0900

    정수 2개 입력받아 그대로 출력하기

--- src/basic100/IntOutput2_1013.java
+++ src/basic100/IntOutput2_1013.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1013 {
+public class IntOutput2_1013 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
