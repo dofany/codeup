490281ef65a5f50df118584f60eafc63959de5d8
dofany <kimdohwan17@gmail.com>
2020-12-30 15:44:37 +0900

    정수 1개 입력받아 평가 출력하기

--- src/basic100/Evaluation_1068.java
+++ src/basic100/Evaluation_1068.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1068 {
+public class Evaluation_1068 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
