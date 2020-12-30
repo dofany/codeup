f5ac402682b663e43a680d4866f56a2ae148c575
dofany <kimdohwan17@gmail.com>
2020-12-30 15:41:30 +0900

    정수 3개 입력받아 짝수만 출력하기

--- src/basic100/EvenOutput_1065.java
+++ src/basic100/EvenOutput_1065.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1065 {
+public class EvenOutput_1065 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
