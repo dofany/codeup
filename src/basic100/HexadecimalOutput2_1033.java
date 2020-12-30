ed819b2f792e8da5d354f74002fdc85a8ea8f5f2
dofany <kimdohwan17@gmail.com>
2020-12-30 14:58:20 +0900

    10진 정수 입력받아 16진수로 출력하기2

--- src/basic100/HexadecimalOutput2_1033.java
+++ src/basic100/HexadecimalOutput2_1033.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1033 {
+public class HexadecimalOutput2_1033 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
