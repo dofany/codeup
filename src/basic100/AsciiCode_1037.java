844c9c46132fdffe9b8f972da0bcd5aa91532ab4
dofany <kimdohwan17@gmail.com>
2020-12-30 15:03:27 +0900

    정수 입력받아 아스키문자로 출력하기

--- src/basic100/AsciiCode_1037.java
+++ src/basic100/AsciiCode_1037.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1037 {
+public class AsciiCode_1037 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
