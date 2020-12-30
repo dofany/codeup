0c6eacb1f1665aac0db48ebf42f0ed09637dab4e
dofany <kimdohwan17@gmail.com>
2020-12-30 15:53:45 +0900

    짝수 합 구하기

--- src/basic100/EvenSum_1078.java
+++ src/basic100/EvenSum_1078.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1078 {
+public class EvenSum_1078 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
