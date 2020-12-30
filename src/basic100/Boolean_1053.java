2c71dc7e6376d5ded1689662f4326d4cd75c784b
dofany <kimdohwan17@gmail.com>
2020-12-30 15:21:57 +0900

    참 거짓 바꾸기

--- src/basic100/Boolean_1053.java
+++ src/basic100/Boolean_1053.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1053 {
+public class Boolean_1053 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
