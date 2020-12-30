4084bc91470a1279b54a0add6733c380e2207eb7
dofany <kimdohwan17@gmail.com>
2020-12-30 15:04:56 +0900

    정수 2개 입력받아 합 출력하기2

--- src/basic100/IntSum2_1039.java
+++ src/basic100/IntSum2_1039.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1039 {
+public class IntSum2_1039 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long a = sc.nextLong();
