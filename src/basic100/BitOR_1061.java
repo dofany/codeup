10f43c6285f09bd842f305f1fd8d71c48e63c916
dofany <kimdohwan17@gmail.com>
2020-12-30 15:37:16 +0900

    비트단위로 OR하여 출력하기

--- src/basic100/BitOR_1061.java
+++ src/basic100/BitOR_1061.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1061 {
+public class BitOR_1061 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
