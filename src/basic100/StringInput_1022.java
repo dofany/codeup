443e96e6d05ec275a88b9a61ccfbc75d78d6cc48
dofany <kimdohwan17@gmail.com>
2020-12-30 14:41:45 +0900

    문장 1개 입력받아 그대로 출력하기

--- src/basic100/StringInput_1022.java
+++ src/basic100/StringInput_1022.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1022 {
+public class StringInput_1022 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
