b4fa188f3bfccb1c0edf0908d86f0a277df416fd
dofany <kimdohwan17@gmail.com>
2020-12-30 16:01:42 +0900

    소리 파일 저장용량 계산하기

--- src/basic100/StorageCapacity_1085.java
+++ src/basic100/StorageCapacity_1085.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1085 {
+public class StorageCapacity_1085 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long h = sc.nextInt();
