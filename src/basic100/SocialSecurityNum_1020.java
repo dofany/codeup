1e68486af9496cb1673d30a96117462d4908223d
dofany <kimdohwan17@gmail.com>
2020-12-30 14:40:11 +0900

    주민번호 입력받아 형태 바꿔 출력하기

--- src/basic100/SocialSecurityNum_1020.java
+++ src/basic100/SocialSecurityNum_1020.java
@@ -3,7 +3,7 @@ package basic100;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class InputOutput_1020 {
+public class SocialSecurityNum_1020 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String num = sc.nextLine();
