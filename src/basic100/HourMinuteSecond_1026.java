5af20df265626cd58492d09f6740910e98bbeb18
dofany <kimdohwan17@gmail.com>
2020-12-30 14:50:08 +0900

    시분초 입력받아 분만 출력하기

--- src/basic100/HourMinuteSecond_1026.java
+++ src/basic100/HourMinuteSecond_1026.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1026 {
+public class HourMinuteSecond_1026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String st = sc.nextLine();
