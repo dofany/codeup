53937c9f68435f06e6642f753d93b415ab8265c9
dofany <kimdohwan17@gmail.com>
2020-12-30 16:12:01 +0900

    설탕과자 뽑기

--- src/basic100/SugarSnack_1098.java
+++ src/basic100/SugarSnack_1098.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1098 {
+public class SugarSnack_1098 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
