470f7ac84b8871e9c4952cae4873022f7e787cfc
dofany <kimdohwan17@gmail.com>
2020-12-30 15:58:22 +0900

    빛 섞어 색 만들기

--- src/basic100/Color_1084.java
+++ src/basic100/Color_1084.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.io.*;
 
-public class InputOutput_1084 {
+public class Color_1084 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
