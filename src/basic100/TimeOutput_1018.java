b6b81c2d2087a553b0aab9470e07716a38e19505
dofany <kimdohwan17@gmail.com>
2020-12-30 14:38:10 +0900

    시간 입력받아 그대로 출력하기

--- src/basic100/TimeOutput_1018.java
+++ src/basic100/TimeOutput_1018.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1018 {
+public class TimeOutput_1018 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String time = sc.nextLine();
