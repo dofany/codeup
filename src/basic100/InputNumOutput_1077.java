7336d3b53786619bbc78242d90268033b2436cea
dofany <kimdohwan17@gmail.com>
2020-12-30 15:51:47 +0900

    정수 1개 입력받아 그 수까지 출력하기

--- src/basic100/InputNumOutput_1077.java
+++ src/basic100/InputNumOutput_1077.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1077 {
+public class InputNumOutput_1077 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
