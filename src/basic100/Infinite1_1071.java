402eba0ec44e3f90a1711fe61cd3e0b6ad7b6b94
dofany <kimdohwan17@gmail.com>
2020-12-30 15:47:58 +0900

    0 입력될 때까지 무한 출력하기1

--- src/basic100/Infinite1_1071.java
+++ src/basic100/Infinite1_1071.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1071 {
+public class Infinite1_1071 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(true){
