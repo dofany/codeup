6fd8444b89e30a83724ca5fc28207773b5cc4818
dofany <kimdohwan17@gmail.com>
2020-12-30 16:09:42 +0900

    이상한 출석 번호 부르기1

--- src/basic100/AttendanceNum1_1093.java
+++ src/basic100/AttendanceNum1_1093.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1093 {
+public class AttendanceNum1_1093 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int array[] = new int[24];
