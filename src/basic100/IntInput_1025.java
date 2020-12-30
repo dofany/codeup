75efc4f6ac5e29245bffb9d7560727934b80e80f
dofany <kimdohwan17@gmail.com>
2020-12-30 14:47:33 +0900

    정수 1개 입력받아 나누어 출력하기

--- src/basic100/IntInput_1025.java
+++ src/basic100/IntInput_1025.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1025 {
+public class IntInput_1025 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
