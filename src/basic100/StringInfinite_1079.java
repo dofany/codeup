6708c4ce9f9ce868ca818d93063c0ad2d23643f0
dofany <kimdohwan17@gmail.com>
2020-12-30 15:54:48 +0900

    원하는 문자가 입력될 때까지 반복 출력하기

--- src/basic100/StringInfinite_1079.java
+++ src/basic100/StringInfinite_1079.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1079 {
+public class StringInfinite_1079 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while(true){
