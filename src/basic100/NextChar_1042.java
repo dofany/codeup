aa694d5e8f0f762d6b800a906d5c83db8c9286b3
dofany <kimdohwan17@gmail.com>
2020-12-30 15:09:35 +0900

    정수 2개 입력받아 나눈 몫 출력하기

--- src/basic100/NextChar_1042.java
+++ src/basic100/NextChar_1042.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1042 {
+public class NextChar_1042 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
