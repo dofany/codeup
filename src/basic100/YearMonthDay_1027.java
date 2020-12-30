f0b41179bc6afa7a2843cfa1b732d4538dd872aa
dofany <kimdohwan17@gmail.com>
2020-12-30 14:50:48 +0900

    년월일 입력받아 형식 바꿔 출력하기

--- src/basic100/YearMonthDay_1027.java
+++ src/basic100/YearMonthDay_1027.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1027 {
+public class YearMonthDay_1027 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
