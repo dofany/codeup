64fe7aafc288bbe095b195cc19027e5f5ba629cb
dofany <kimdohwan17@gmail.com>
2020-12-30 14:31:47 +0900

    출력하기 7

--- src/basic100/Output7_1007.java
+++ src/basic100/Output7_1007.java
@@ -1,6 +1,6 @@
 package basic100;
 
-public class Output_1007 {
+public class Output7_1007 {
     public static void main(String[] args) {
         System.out.println("\"C:\\Download\\hello.cpp\"");
     }
