84480ef609bce8db15fb7968e1773628151c644f
dofany <kimdohwan17@gmail.com>
2020-12-30 14:43:48 +0900

    단어 1개 입력받아 나누어 출력하기

--- src/basic100/WordInput_1024.java
+++ src/basic100/WordInput_1024.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1024 {
+public class WordInput_1024 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
