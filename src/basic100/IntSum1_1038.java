c7ca744417e78a91e723fd524df22f626210d2fd
dofany <kimdohwan17@gmail.com>
2020-12-30 15:04:16 +0900

    정수 2개 입력받아 합 출력하기1

--- src/basic100/IntSum1_1038.java
+++ src/basic100/IntSum1_1038.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1038 {
+public class IntSum1_1038 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextInt();
