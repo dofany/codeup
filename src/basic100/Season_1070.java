a78fec8ce96bc7abdaeab6eb23b2235105782dde
dofany <kimdohwan17@gmail.com>
2020-12-30 15:46:37 +0900

    월 입력받아 계절 출력하기

--- src/basic100/Season_1070.java
+++ src/basic100/Season_1070.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1070 {
+public class Season_1070 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
