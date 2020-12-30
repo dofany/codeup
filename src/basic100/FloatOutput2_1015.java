b436010fae430a55404f3621685f5af63a49f6c4
dofany <kimdohwan17@gmail.com>
2020-12-30 14:36:06 +0900

    실수 입력받아 둘째 자리까지 출력하기

--- src/basic100/FloatOutput2_1015.java
+++ src/basic100/FloatOutput2_1015.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1015 {
+public class FloatOutput2_1015 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float f = sc.nextFloat();
