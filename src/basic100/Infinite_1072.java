e82f10fff8abe4ffc52dc10f0bbc68cd56eae9d8
dofany <kimdohwan17@gmail.com>
2020-12-30 15:48:35 +0900

    정수 입력받아 계속 출력하기

--- src/basic100/Infinite_1072.java
+++ src/basic100/Infinite_1072.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1072 {
+public class Infinite_1072 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
