7ad734f9d5e12eb14d67d6ef63647bfee56bcdaf
dofany <kimdohwan17@gmail.com>
2020-12-30 15:39:02 +0900

    두 정수 입력받아 큰 수 출력하기

--- src/basic100/BitNum_1063.java
+++ src/basic100/BitNum_1063.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1063 {
+public class BitNum_1063 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
