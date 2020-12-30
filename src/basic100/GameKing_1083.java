ed99d4f4bb367ed5beea627e258aca42c15f186f
dofany <kimdohwan17@gmail.com>
2020-12-30 15:57:56 +0900

    3 6 9 게임의 왕이 되자!

--- src/basic100/GameKing_1083.java
+++ src/basic100/GameKing_1083.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1083 {
+public class GameKing_1083 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
