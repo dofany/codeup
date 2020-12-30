980b3780cb139785a49ec950d80bcd59903d179c
dofany <kimdohwan17@gmail.com>
2020-12-30 15:56:12 +0900

    주사위를 2개 던지면?

--- src/basic100/Dice_1081.java
+++ src/basic100/Dice_1081.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1081 {
+public class Dice_1081 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i,j;
