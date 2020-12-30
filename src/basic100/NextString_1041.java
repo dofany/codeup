f1b1cfd4c4ac00b4a6e1a69b1a728e874952ffd9
dofany <kimdohwan17@gmail.com>
2020-12-30 15:08:37 +0900

    문자 1개 입력받아 다음 문자 출력하기

--- src/basic100/NextString_1041.java
+++ src/basic100/NextString_1041.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1041 {
+public class NextString_1041 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
