b0b962ad62fb761d61af8daa869a7c2df134dcbd
dofany <kimdohwan17@gmail.com>
2020-12-30 14:53:20 +0900

    정수 1개 입력받아 그대로 출력하기3

--- src/basic100/IntInput3_1030.java
+++ src/basic100/IntInput3_1030.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1030 {
+public class IntInput3_1030 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long l= sc.nextLong();
