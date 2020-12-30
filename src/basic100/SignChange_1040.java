c2d8f2be7acbb8934eb413389bfa2593deef3137
dofany <kimdohwan17@gmail.com>
2020-12-30 15:06:51 +0900

    정수 1개 입력받아 부호 바꿔 출력하기

--- src/basic100/SignChange_1040.java
+++ src/basic100/SignChange_1040.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1040 {
+public class SignChange_1040 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
