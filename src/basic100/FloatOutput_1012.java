95473cf0f9dc320dfb2df3eda8061ebcac1970da
dofany <kimdohwan17@gmail.com>
2020-12-30 14:33:55 +0900

    실수 1개 입력받아 그대로 출력하기

--- src/basic100/FloatOutput_1012.java
+++ src/basic100/FloatOutput_1012.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1012 {
+public class FloatOutput_1012 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float f = sc.nextFloat();
