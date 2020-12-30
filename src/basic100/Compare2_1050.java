6d79491d573dd8861e02beefc1650dcf48eb142d
dofany <kimdohwan17@gmail.com>
2020-12-30 15:20:40 +0900

    두 정수 입력받아 비교하기2

--- src/basic100/Compare2_1050.java
+++ src/basic100/Compare2_1050.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1050 {
+public class Compare2_1050 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
