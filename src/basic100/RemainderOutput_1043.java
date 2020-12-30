0706651a6dfff2804603d24c5e1745d37a10340b
dofany <kimdohwan17@gmail.com>
2020-12-30 15:11:28 +0900

    정수 2개 입력받아 나눈 나머지 출력하기

--- src/basic100/RemainderOutput_1043.java
+++ src/basic100/RemainderOutput_1043.java
@@ -2,7 +2,7 @@ package basic100;
 
 import java.util.Scanner;
 
-public class InputOutput_1043 {
+public class RemainderOutput_1043 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
