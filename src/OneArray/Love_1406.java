d4aa893a956ca5aa0dbc22694653c8d4d495be4c
dofany <kimdohwan17@gmail.com>
2020-12-31 14:12:37 +0900

    love

---
+++ src/OneArray/Love_1406.java
@@ -0,0 +1,17 @@
+package OneArray;
+
+import java.util.Scanner;
+
+public class Love_1406 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        switch (s){
+            case "love" :
+                System.out.print("I love you.");
+                break;
+            default :
+                break;
+        }
+    }
+}
