18b789f6dfaf94cdb13db9c54fdc79bd8739fe23
dofany <kimdohwan17@gmail.com>
2021-01-04 16:50:27 +0900

    암호 처리

---
+++ src/OneArray/Password_1408.java
@@ -0,0 +1,19 @@
+package OneArray;
+
+import java.util.Scanner;
+
+public class Password_1408 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder();
+        str.append(sc.nextLine());
+
+        for(int i = 0; i<str.length(); i++){
+            System.out.print((char)(str.charAt(i)+2));
+        }
+        System.out.println();
+        for(int i = 0; i<str.length();i++){
+            System.out.print((char)((str.charAt(i)*7)%80+48));
+        }
+    }
+}
