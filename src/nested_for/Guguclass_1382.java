949eee040925be5a1ac23399967e6de21540ed2b
dofany <kimdohwan17@gmail.com>
2021-01-17 21:06:04 +0900

    Guguclass

---
+++ src/nested_for/Guguclass_1382.java
@@ -0,0 +1,14 @@
+package nested_for;
+
+public class Guguclass_1382 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 5; j++) {
+
+                System.out.printf("%d x %d = %2d\t", j, i, j * i);
+
+            }
+            System.out.println();
+        }
+    }
+}
