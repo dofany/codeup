183e8b68290f909977fa4e908113b23473e9b75d
dofany <kimdohwan17@gmail.com>
2021-01-31 19:56:59 +0900

    welcome!

---
+++ src/string/Welcome_1734.java
@@ -0,0 +1,11 @@
+package string;
+
+import java.util.Scanner;
+
+public class Welcome_1734 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.printf("welcome! %s",s);
+    }
+}
