d19e5af8ed8efbc7e3f949b89e7e749190b5170d
dofany <kimdohwan17@gmail.com>
2021-03-15 18:20:17 +0900

    2차원 배열 지그재그 채우기 2-4

---
+++ src/TwoArray/TwoArray_2_4_1471.java
@@ -0,0 +1,31 @@
+package TwoArray;
+
+import java.util.Scanner;
+
+public class TwoArray_2_4_1471 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[][] = new int[101][101];
+        int x = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                for (int j = n; j >= 1; j--) {
+                    array[j][i] = x++;
+                }
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    array[j][i] = x++;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
